825d53adccc064d2b84011480e3fbee2ad6ce61a
Kilian Voss <kilian96@live.de>
2023-03-02 16:45:10 +1100

    outsourcing and sketch 4 data restructuring

---
+++ Bulk_Mail_Generator.py
@@ -0,0 +1,11 @@
+# Construct Email w CV
+def generate_Email(listing_essential_info_dict):
+    email_Text = 0
+    return email_Text
+    
+    # Load CV
+def Generate_emails(list_of_all_listings):
+    
+
+
+    pass
--- Test.py
+++ Test.py
@@ -73,15 +73,15 @@ def generate_price_statistics(list_of_all_listings):
 essential_info_dicts = []
 
 
-def Generate_emails(list_of_all_listings):
-    
-    # Construct Email w CV
-    def generate_Email(listing_essential_info_dict):
-        email_Text = 0
-        return email_Text
-    
-    # Load CV
-
+# Generate Essential info dicts
+# Titel
+# Type
+# Address
+# RealEstate Agency
+# Agent Name
+# Agent Email/Profile
+# Listing URL
+for listing in filtered_listings:
     pass
 
 
