b6ab07fe8f8b4f35a88eeb2a5bfed9ea797c2594
Kilian Voss <kilian96@live.de>
2023-03-04 13:29:13 +1100

    DKN

--- Test.py
+++ Test.py
@@ -1,7 +1,11 @@
 import re
-import statistics
 from realestate_data import Search, Locality, PriceRange, Filters, paged_results
 p = PriceRange()
+listings_for_all_postcodes = []
+list_of_postcodes = ['3000', '3001', '3002', '3003', '3004', '3005', '3006', '3008', '3010', '3011', '3012', '3013', '3015', '3016', '3018', '3019', '3020', '3021', '3022', '3023', '3024', '3025', '3026', '3027', '3028', '3029', '3030', '3031', '3032', '3033', '3034', '3036', '3037', '3038', '3039', '3040', '3041', '3042', '3043', '3044', '3045', '3046', '3047', '3048', '3049', '3050']
+
+for code in list_of_postcodes: 
+    pass
 
 l = Locality()
 l.locality = 'Melbourne'
@@ -22,70 +26,66 @@ all_data = []
 for page in paged_data:
     if page[0]['count'] > 0:
         for listing in page[0]['results']:
-            all_data.append(listing)
+            if any(char.isdigit() for char in listing['price']['display']):
+                extract_number = int(re.findall('[\d,]+', listing['price']['display'].replace(',', ''))[0])
+                listing['price'] = extract_number
+                all_data.append(listing)
     elif len(page) > 1 and page[1]['count'] > 0:
         for listing in page[1]['results']:
-            all_data.append(listing)
+            if any(char.isdigit() for char in listing['price']['display']):
+                extract_number = int(re.findall('[\d,]+', listing['price']['display'].replace(',', ''))[0])
+                listing['price'] = extract_number
+                all_data.append(listing)
     else:
         pass
 
- # format price to int
-formated_listings=[]
-for index,listing in enumerate(all_data):
-   
-    if any(char.isdigit() for char in listing['price']['display']):
-        # extract_number = int(re.findall('\d+', listing['price']['display'])[0])
-        extract_number = int(re.findall('[\d,]+', listing['price']['display'].replace(',', ''))[0])
-        
-        listing['price'] = extract_number
-        formated_listings.append(listing)
 
-def filtaaa(listing):    
-    max_price=900
-    min_bedroom=3
-    if listing['price'] <= max_price and listing['features']['general']['bedrooms'] >= min_bedroom:
+
+def filter_bedrooms_and_price(listing):    
+    if listing['price'] <= 900 and listing['features']['general']['bedrooms'] >= 3:
+        return True
+    elif listing['price'] <= 600 and listing['features']['general']['bedrooms'] >= 2:
+        return True
+    elif listing['price'] <= 300 and listing['features']['general']['bedrooms'] >= 1:
         return True
     else:
         return False
 
-filtered_listings_please_extract = filter(filtaaa, formated_listings)
+filtered_listings_please_extract = filter(filter_bedrooms_and_price, all_data)
 filtered_listings = list(filtered_listings_please_extract)
 
-def generate_price_statistics(list_of_all_listings):
-    # all bond prices
-    bonds = []
-    # all rent prices
-    rents = []
-    for listing in list_of_all_listings:
-    # for listing in filtered_listings:
-        if 'price' in listing:
-            rents.append(listing['price'])
-        if 'bond' in listing:
-            bonds.append(listing['bond']['value'])
 
-    return {
-        'average_rent_value' : statistics.mean(rents),
-        'median_rent_value' : statistics.median(rents),
-        'average_bond_value' : statistics.mean(bonds),
-        'median_bond_value' : statistics.median(bonds)
-    }
 
 essential_info_dicts = []
 
-
 # Generate Essential info dicts
-# Titel
-# Type
-# Address
-# RealEstate Agency
-# Agent Name
-# Agent Email/Profile
-# Listing URL
 for listing in filtered_listings:
-    pass
+    
+    essential_info_dicts.append({
+        'title': listing.get('title', 0),
+        'type': listing.get('propertyType', 0),
+        'address': listing.get('address', 0),
+        'address_str': listing.get('address', 0).get('streetAddress') + ' in ' + listing.get('address', 0).get('locality'),
+        'bed_rooms': listing.get('features',{}).get('general',{}).get('bedrooms',0),
+        'bond': listing.get('bond', {}).get('value', 0),
+        'rent': listing.get('price', 0),
+        'rent_per_room': listing.get('price', 0)/listing.get('features',{}).get('general',{}).get('bedrooms',0),
+        'agency': listing.get('agency', {}).get('name', 0),
+        'agent_name': listing.get('lister', {}).get('name', 0),
+        'agent_mail': listing.get('lister', {}).get('email', 0),
+        'url': listing.get('_links', {}).get('prettyUrl', {}).get('href', 0),
+        'listing_id': listing['listingId']
+    })
 
 
 # Save all of the above listings in a database
+
+# Generate Emails for each listing
+from Bulk_Mail_Sender import EmailSender
+Bulk_Email_Instance = EmailSender()
+Bulk_Email_Instance.generating_all_emails(essential_info_dicts)
+Bulk_Email_Instance.preparing_all_emails(essential_info_dicts)
+# Bulk_Email_Instance.send_all_emails()
 
 L=0
 
